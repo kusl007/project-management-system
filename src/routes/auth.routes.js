a72be5f133dc7ca0a211736fa959da4ec1806f08
kusl007 <kushal.bansal.1001@gmail.com>
2025-08-16 20:28:21 +0545

    validation added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
+import { validate } from "../middlewares/validator.middleware.js";
+import { registerUser } from "../controllers/auth.controllers.js";
 import {
-
-  registerUser
- 
-} from "../controllers/auth.controllers.js";
-
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgotPasswordValidator,
+} from "../validators/index.js";
 
 const router = Router();
 
 // unsecured route
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+
 
 export default router;
