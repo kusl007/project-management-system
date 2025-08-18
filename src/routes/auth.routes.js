3658df5dad64ddac7407ee3248184ecc612a8561
kusl007 <kushal.bansal.1001@gmail.com>
2025-08-18 12:09:59 +0545

    password related controller done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -25,10 +25,25 @@ router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, loginUser);
 router.route("/verify-email/:verificationToken").get(verifyEmail);
 router.route("/refresh-token").post(refreshAccessToken);
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
 
 //secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
 router
   .route("/resend-email-verification")
   .post(verifyJWT, resendEmailVerification);
